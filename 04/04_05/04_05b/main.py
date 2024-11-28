d16747732e41cb63f473342494694716a8e95561
John Moremm L. Abuyabor <john.moremm@gmail.com>
2024-11-28 18:10:12 +0000

    Challenge: Using sets to check if a string has a unique set of characters

--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,5 +1,13 @@
 def has_unique_characters(data):
-    return False
+    set_of_letters = set([]);
+
+    for letter in data:
+        if (letter not in set_of_letters):
+            set_of_letters.add(letter)
+        else:
+            return False
+    
+    return True
 
 print(has_unique_characters('sample'))
 print(has_unique_characters('hello world'))
