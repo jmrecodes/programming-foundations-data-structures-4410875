b841cab39efbbbde58825f98312ebc1043033009
John Moremm L. Abuyabor <john.moremm@gmail.com>
2024-11-28 18:12:11 +0000

    Shorter method

--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,13 +1,17 @@
 def has_unique_characters(data):
-    set_of_letters = set([]);
+    # set_of_letters = set([]);
 
-    for letter in data:
-        if (letter not in set_of_letters):
-            set_of_letters.add(letter)
-        else:
-            return False
+    # for letter in data:
+    #     if (letter not in set_of_letters):
+    #         set_of_letters.add(letter)
+    #     else:
+    #         return False
     
-    return True
+    # return True
+
+    unique_data = set(data)
+
+    return len(unique_data) == len(data)
 
 print(has_unique_characters('sample'))
 print(has_unique_characters('hello world'))
