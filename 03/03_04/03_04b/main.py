b81492516b97c5d3d8e747df7565033d9fbd294b
John Moremm L. Abuyabor <john.moremm@gmail.com>
2024-11-28 17:38:34 +0000

    Using Comphrehensions to improve Time Complexity from O(n) to ?

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -8,12 +8,13 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    new_user_pref = {}
-    for key, value in user_pref.items():
-        if (value != None):
-            new_user_pref[key] = value
+    return { key: value for key, value in user_pref.items() if value is not None }
+    # new_user_pref = {}
+    # for key, value in user_pref.items():
+    #     if (value != None):
+    #         new_user_pref[key] = value
 
-    return new_user_pref
+    # return new_user_pref
 
 
 print(update_preferences(user_preferences))
