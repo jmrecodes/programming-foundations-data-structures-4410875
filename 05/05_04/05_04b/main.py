e0abe8abc8531314cb8a82b91c6b7f3674f4db2f
John Moremm L. Abuyabor <john.moremm@gmail.com>
2024-12-05 03:14:09 +0000

    Add comments to Binary challege

--- 05/05_04/05_04b/main.py
+++ 05/05_04/05_04b/main.py
@@ -9,21 +9,29 @@ def print_queue(q):
 def num_to_binary(num):
   binary_digit = [0]
   while (num > 0):
+    # If last digit is 0, simply set it to 1
     if (binary_digit[len(binary_digit) - 1] == 0):
       binary_digit[len(binary_digit) - 1] = 1
     else:
+      # Complex carry-over mechanism
       bd_len = len(binary_digit) - 1
-      
       need_to_add = True
+
+      # Traverse digits from right to left
       while (bd_len > 0) and need_to_add:
         if (binary_digit[bd_len] == 1):
+          # If digit is 1, set to 0 and continue
+
           binary_digit[bd_len] = 0
         else:
+          # If digit is 0, set to 1 and stop carry-over
+
           binary_digit[bd_len] = 1
           need_to_add = False
 
         bd_len -= 1
       
+      # If need to add new digit
       if (need_to_add):
         binary_digit.append(0)
 
@@ -34,7 +42,10 @@ def num_to_binary(num):
 
 def decimal_binary_all_below(num):
   binaries = deque()
+
   start = 1
+
+  # Traverse from 1 up to the input number
   while (start <= num):
     binaries.append(num_to_binary(start))
 
@@ -42,4 +53,5 @@ def decimal_binary_all_below(num):
   
   print_queue(binaries)
 
+# Print from 1 to 14 all of its binary representations
 decimal_binary_all_below(14)
